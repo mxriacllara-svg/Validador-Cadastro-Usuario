17503285b88151933b2607d2e769a145f9302d2b
Maria <mxriacllara@gmail.com>
2026-09-03 17:27:43 -0300

    Primeiro commit - validador de cadastro

---
+++ src/main/java/com/mycompany/validadorcadastrousuario/ValidadorCadastroUsuario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.validadorcadastrousuario;
+
+/**
+ *
+ * @author mxria
+ */
+public class ValidadorCadastroUsuario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
